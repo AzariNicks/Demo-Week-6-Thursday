72049d23bd6bc995dbd66c13957d4f44c5e64127
AzariNicks <azari.nicks99@gmail.com>
2022-04-28 17:25:03 -0400

    Thursday

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const path = require('path')
 
 const app = express() 
 
+
+
+
+
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
   accessToken: '17c9883710fa4d0987e35e5de81ad841',
@@ -12,16 +16,13 @@ var rollbar = new Rollbar({
 })
 
 // record a generic message and send it to Rollbar
-rollbar.info('Hello we chillin world!')
-rollbar.log('Hello we chillin world!')
-
-
 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-const port = process.env.PORT || 4545
+const port = process.env.PORT || 4545 
+const lol = 90
 
 app.listen(port, () => console.log(`Take us to warp ${port}!`))
